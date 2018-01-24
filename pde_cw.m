fcff63cd9966cd0877551c0f27a2a576196105ed
zceehaq <zceehaq@CT00018695.ad.ucl.ac.uk>
2018-01-24 21:50:37 +0000

    progress

--- pde_cw.m
+++ pde_cw.m
@@ -1,24 +1,34 @@
-r_max = 0.01;
-n_max = 100;
-t_max = 100;
+clearvars
+close all
+
+
 D = 1.2e-7;
 G = 2.4e-3;
 T_inital = 20;
 T_ref = 4;
 
-Nr=100;
-Nt=100;
-r=linspace(0,r_max,Nr); 
-t=linspace(0,t_max,Nt); 
+r_max = 0.01;
+t_max = 100;
+n_max=100; % Define maximum value of k
+Nr=101; % Define number of x values between 0 and L
+Nt=101; % Define number of y values between 0 and L
 
-[R,T]=meshgrid(r,t); 
-Vk=zeros(Nt,Nr,n_max); 
+r=linspace(0,r_max,Nr); % Define range of x values
+t=linspace(0,t_max,Nt); % Define range of y values
+[R,T]=meshgrid(r,t); % Defines grids for x and y
+Uk=zeros(Nt,Nr,n_max); % Initialise Tk
 
-for n = 1:n_max
-    Vk(:,:,n)= 2.*r_max./(pi.*R) .* (((-1)^n)/n .* sin(n*pi.*R/r_max) .* exp((n^2.*pi^2)*D.*T/r_max^2)) + T_ref + r_max^2.*G/(6.*D) - R.^2 .* G/(6.*D);
+% Calculate Ny by Nx grid of Tk for each value of k
+for n=1:n_max
+ Uk(:,:,n)=2.*r_max./(pi.*R) .* (((-1)^n)/n .* sin(n*pi.*R/r_max) .* exp((n^2.*pi^2)*D.*T/r_max^2)) + T_ref + r_max^2.*G/(6.*D) - R.^2 .* G/(6.*D);
 end
 
-N=sum(Vk,3);
-
-figure;
-mesh(R,T,Vk);
+% Sum Tk along the 3rd dimension to obtain k
+U=sum(Uk,3);
+figure
+mesh(R,U,T)
+xlabel('radius of apple (m)')
+ylabel('Time (s)')
+zlabel('temperature (c)')
+% axis([0 L 0 L 0 1.5*A])
+% colorbar
